468695f0f7b1b15cf89b0fae749e5f1a82062ec5
Zakhar1712 <zaxarferenz@gmail.com>
2024-04-30 22:32:55 +0300

    add commit

---
+++ 1/1.js
@@ -0,0 +1,65 @@
+let a = 'hello'
+console.log(a)
+
+let b = 'owu'
+console.log(b)
+
+let c = 'com'
+console.log(c)
+
+let d = 'ua'
+console.log(d)
+
+let a1 = 1
+console.log(a1)
+
+let a2 = 10
+console.log(a2)
+
+let a3 = -999
+console.log(a3)
+
+let a4 = 123
+console.log(a4)
+
+let a5 = 3.14
+console.log(a5)
+
+let a6 = 2.7
+console.log(a6)
+
+let a7 = 16
+console.log(a7)
+
+let b1 = true
+console.log(b1)
+
+let b2 = false
+console.log(b2)
+
+let arr = [11, 22, 33, 44]
+let firstName: = {
+    name:tom,
+    surname:siv,
+    middle name:Ivan,
+}
+console.log(arr)
+console.log(firstName)
+
+let arr1 = [11, 22, 33, 44]
+let middleName: = {
+    name:Tom,
+    surname:si,
+    middle name:Iva,
+}
+console.log(arr1)
+console.log(middleName)
+
+let arr2 = [11, 22, 33, 44]
+let lastName: = {
+    name:Lina,
+    surname:siva,
+    middle name:Ivann,
+}
+console.log(arr2)
+console.log(lastName)
